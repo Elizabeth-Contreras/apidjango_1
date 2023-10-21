60a24dbb2ec899a2917d31bca40e6bc8f141d2b1
Elizabeth-Contreras <144398838+Elizabeth-Contreras@users.noreply.github.com>
2023-10-21 17:06:19 -0600

    Update settings.py

--- API9ISC22/API9ISC22/settings.py
+++ API9ISC22/API9ISC22/settings.py
@@ -133,6 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'elicontreras201@gmail.com'
-EMAIL_HOST_PASSWORD = 'zqezcwybfsbvbvwe'
 EMAIL_USE_TLS = True
