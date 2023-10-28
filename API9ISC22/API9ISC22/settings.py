541568655f097bf247c53c04de2ff1b1fc9b822e
Elizabeth-Contreras <2019452028@teschi.edu.mx>
2023-10-27 18:23:52 -0600

    grafica

--- API9ISC22/API9ISC22/settings.py
+++ API9ISC22/API9ISC22/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 <<<<<<< HEAD
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'elicontreras201@gmail.com'
+EMAIL_HOST_PASSWORD = 'krojfzzngciqvoyj'
 EMAIL_USE_TLS = True
 =======
 EMAIL_USE_TLS = True
